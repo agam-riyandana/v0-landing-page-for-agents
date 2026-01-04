5ecfdfc1bf100b47d5110ce7bb39b0f7723e473b
Agam Riyandana <87102026+agam-riyandana@users.noreply.github.com>
2026-01-05 01:07:20 +0700

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next"
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://bayarkita.com"
+  const baseUrl = "https://my.bayarkita.com"
 
   return [
     {
