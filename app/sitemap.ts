46d3ee4dd72020ae7826bc3674932fcd1c84d06e
Agam Riyandana <87102026+agam-riyandana@users.noreply.github.com>
2026-01-05 01:00:07 +0700

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next"
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://pulsapro.vercel.app"
+  const baseUrl = "https://bayarkita.com"
 
   return [
     {
